e96814ff9ccf7474222c6fb6b229b72070fce963
luojiyin1987 <luojiyin@hotmail.com>
2020-03-09 16:50:47 +0800

    youtube-dl

--- app.js
+++ app.js
@@ -6,14 +6,33 @@ const { exec } = require('child_process');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/download", (req, res)=>{
     console.log("download", req.body);
-    res.send({result:0});
+
+    if(!req.body.URL)
+    {
+        res.send({result:1});
+        return;
+    }
+
+    youtubeDownload(req.body.URL, (error)=> {
+        res.send(error);
+    } )
+    
 })
 
 app.listen(3000, ()=>
 {
    console.log('Sever works');
 });
+
+youtubeDownload=(url, next) =>{
+    const cmd = `youtube-dl  ${url}`;   
+    exec(cmd, (error, stdout, stderr) =>{
+        next({result: Number(!!error), data: error ? stderr : stdout});
+    })
+}
