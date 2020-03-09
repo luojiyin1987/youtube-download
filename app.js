49a25a7830f554df8ebee8f090362b5c10cab40e
luojiyin <luojiyin@hotmail.com>
2020-03-09 20:09:02 +0800

    static files index

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { exec } = require('child_process');
+const serveIndex = require('serve-index');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
-app.use('/download', express.static(__dirname + "download"));
+app.use('download', express.static('download'), serveIndex('download', {'icons':true}));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
