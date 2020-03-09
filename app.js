9c225670653b6769042d91aedf410e9ca3043489
luojiyin1987 <luojiyin@hotmail.com>
2020-03-09 12:14:57 +0800

    from post get json data

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const { exec } = require('child_process');
 
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/download", (req, res)=>{
+    console.log("download", req.body);
+    res.send({result:0});
+})
 
 app.listen(3000, ()=>
 {
-    console.log('Sever works');
+   console.log('Sever works');
 });
