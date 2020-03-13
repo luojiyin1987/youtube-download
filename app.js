8bcd8b42ca69e9c37c5527f047bfb0f33e7e03a8
luojiyin1987 <luojiyin@hotmail.com>
2020-03-13 16:11:32 +0800

    fix error

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { exec } = require('child_process');
+const serveIndex = require('serve-index');
 
 
 const app = express();
 
 app.use(express.static('public'));
+app.use('download', express.static('download'), serveIndex('download', {'icons':true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
