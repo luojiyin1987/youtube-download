0332b8f27ba84dd4cc721a1d699099b2cf7eea6c
luojiyin1987 <luojiyin@hotmail.com>
2020-03-13 17:41:33 +0800

    debug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { exec } = require('child_process');
-
+const serveIndex = require('serve-index');
 
 const app = express();
 
