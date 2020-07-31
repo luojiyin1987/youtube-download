8c1cc8d325972d1e8b69e1c07712c64dfaa7f32c
“sf” <sf@gmail.com>
2020-07-31 18:43:18 +0800

    refactor: del no use package

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const { exec } = require('child_process');
 const serveIndex = require('serve-index');
 
